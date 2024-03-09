52807d76f69100e62df0912e40f0d2a335b8caae
Hajime <millarayzunigag@gmail.com>
2024-03-09 14:20:32 -0300

    segundo commit cambios en input

--- assets/js/app.js
+++ assets/js/app.js
@@ -45,58 +45,9 @@ form.addEventListener("submit", (e) => {
 
     if (usuarioIngresado === 1 && asuntoIngresado === 1 && mensajeIngresado === 1) {
         document.querySelector(".resultado").innerHTML = "Mensaje enviado con éxito" 
+        document.querySelector("errorNombre").innerHTML = ""
+        document.querySelector("errorAsunto").innerHTML = ""
+        document.querySelector("errorMensaje").innerHTML = ""
     }
 }) 
 
- 
-//FUNCIÓN COMPLETA CON BOTÓN
-
-botonEnviar.addEventListener("click", function() {
-
-})
-
-
-
-
-
-
-// botonEnviar.addEventListener("click" ,function() {
-//   //cuerpo de la función anónnima que se ejectuta al hacer click
-//   const form = document.querySelector("#nombre") 
-//   document.querySelector(".errorNombre").innerHTML = "ERROR:" + form.value 
-//   .errorNombre.style.color = "RED"
-// })  
-
-  
-
-
- //comprobar que los elementos están correctamente seleccionados
-// console.log(form)
-// console.log(nombreUsuario)
-// console.log(asuntoForm)
-// console.log(mensajeForm)
-
-// // detectar el evento submit del formulario
-
-// form.addEventListener("submit", (evento) => {
-//     console.log("procesando información")
-//     evento.preventDefault()
-
-// // almacenamos info en una constante 
-// //1.- nombreUsuario
-// const textUsuario = nombreUsuario.value 
-
-// const regexNombreUsuario = /[a-zA-Z]/gim
-// if(regexNombreUsuario.test(textUsuario)) {
-
-//     document.writer("pasa adelante")
-
-// } else {
-//     document.write("se necesita información")
-
-// }
-
-
-
-
-// })
